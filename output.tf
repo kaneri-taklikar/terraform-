75b109dfd922e57b30fd35c68f4a01329ff26edd
kaneri <kaneritaklikar7@gmail.com>
2025-11-19 12:55:14 +0530

    Fix output variable references for instance IPs

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 
 output "aws_instance_public_ip" {
 description = "this is my instance public ip"
-  value = aws_instance.practice-instance.public_ip
+  value = aws_instance.practice_instance.public_ip
 }
 
 output "aws_instance_private_ip" {
 description =" this is my priveta ip"
-  value = aws_instance.practice-instance.private_ip
+  value = aws_instance.practice_instance.private_ip
 }
 
