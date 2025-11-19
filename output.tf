fe5c2d4166da15ac8121f2536041de7d6fdbe182
kaneri <kaneritaklikar7@gmail.com>
2025-11-19 12:44:01 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,8 @@
-output "aws_instance_public_ip" {
-    description = "the piblic ip address of our ec2 instace"
-    value = aws_instance.my-practice-instance.public_ip
 
+output "aws_instance_public_ip" {
+  value = aws_instance.my-practice-instance.public_ip
 }
 
 output "aws_instance_private_ip" {
-    description = " this is the private ip address"
-    value = aws_instance.my-practice-instance.private_ip
+  value = aws_instance.my-practice-instance.private_ip
 }
