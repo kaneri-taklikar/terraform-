49cf4b0f328b81d6b396765861999e1bcf9641e2
kaneri <kaneritaklikar7@gmail.com>
2025-11-19 13:01:24 +0530

    Remove descriptions from output IP variables
    Removed descriptions from output variables for IP addresses.

--- output.tf
+++ output.tf
@@ -1,11 +1,9 @@
 
+
 output "aws_instance_public_ip" {
-description = "this is my instance public ip"
   value = aws_instance.practice_instance.public_ip
 }
 
 output "aws_instance_private_ip" {
-description =" this is my priveta ip"
   value = aws_instance.practice_instance.private_ip
 }
-
