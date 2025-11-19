c0176ec95941babefe6bf9259432934f90245fd7
kaneri <kaneritaklikar7@gmail.com>
2025-11-19 12:38:23 +0530

    Fix output value for AWS instance private IP

--- output.tf
+++ output.tf
@@ -6,5 +6,5 @@ output "aws_instance_public_ip" {
 
 output "aws_instance_private_ip" {
     description = " this is the private ip address"
-    value = aws_instance.my-practice-instance.aws_instance_private_ip
+    value = aws_instance.my-practice-instance.private_ip
 }
